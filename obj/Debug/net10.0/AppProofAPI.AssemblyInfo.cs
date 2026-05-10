934dc99a5d25eebfbd0fed0b1ee3df4547d640d6
lichiea <lizaear@mail.ru>
2026-05-10 21:06:02 +0300

    5

--- obj/Debug/net10.0/AppProofAPI.AssemblyInfo.cs
+++ obj/Debug/net10.0/AppProofAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppProofAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d1b57dcf28df0e77494b98068fbab622167bb5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppProofAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppProofAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
